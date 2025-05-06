ca99119e6cec163a68781007daf4ec9293e9c526
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-05-06 14:37:01 +0900

    Update MainActivity.java

--- app/src/main/java/com/github/naofum/gogakudroid/MainActivity.java
+++ app/src/main/java/com/github/naofum/gogakudroid/MainActivity.java
@@ -73,11 +73,11 @@ public class MainActivity extends Activity {
 	private AdView adView;
 
 	static {
-//		ENGLISH.put("basic0", "小学生の基礎英語");
+		ENGLISH.put("title", "放送翌週月曜日から1週間");
 		ENGLISH.put("english/basic0", "小学生の基礎英語");
 		ENGLISH.put("english/basic1", "中学生の基礎英語_レベル1");
 		ENGLISH.put("english/basic2", "中学生の基礎英語_レベル2");
-		ENGLISH.put("english/basic3", "中高生の基礎英語_in_English");
+//		ENGLISH.put("english/basic3", "中高生の基礎英語_in_English");
 		ENGLISH.put("english/kaiwa", "ラジオ英会話");
 		ENGLISH.put("english/timetrial", "英会話タイムトライアル");
 		ENGLISH.put("english/business1", "ラジオビジネス英語");
@@ -95,36 +95,31 @@ public class MainActivity extends Activity {
 	}
 	protected static Map<String, String> MULTILINGUAL = new LinkedHashMap<String, String>();
 	static {
+		MULTILINGUAL.put("title", "放送終了後1週間配信");
 		MULTILINGUAL.put("GGQY3M1929", "小学生の基礎英語");
 		MULTILINGUAL.put("148W8XX226", "中学生の基礎英語_レベル1");
 		MULTILINGUAL.put("83RW6PK3GG", "中学生の基礎英語_レベル2");
-		MULTILINGUAL.put("B2J88K328M", "中高生の基礎英語_in_English");
+//		MULTILINGUAL.put("B2J88K328M", "中高生の基礎英語_in_English");
 		MULTILINGUAL.put("PMMJ59J6N2", "ラジオ英会話");
 		MULTILINGUAL.put("8Z6XJ6J415", "英会話タイムトライアル");
 		MULTILINGUAL.put("368315KKP8", "ラジオビジネス英語");
 		MULTILINGUAL.put("BR8Z3NX7XM", "エンジョイ・シンプル・イングリッシュ");
 		MULTILINGUAL.put("77RQWQX1L6", "ニュースで学ぶ「現代英語」");
-		MULTILINGUAL.put("7Y5N5G674R", "ボキャブライダー");
+//		MULTILINGUAL.put("7Y5N5G674R", "ボキャブライダー");
 		MULTILINGUAL.put("983PKQPYN7", "まいにち中国語");
-		MULTILINGUAL.put("MYY93M57V6", "ステップアップ中国語");
+//		MULTILINGUAL.put("MYY93M57V6", "ステップアップ中国語");
 		MULTILINGUAL.put("LR47WW9K14", "まいにちハングル講座");
-       		MULTILINGUAL.put("NLJM5V3WXK", "ステップアップハングル講座");
+//     		MULTILINGUAL.put("NLJM5V3WXK", "ステップアップハングル講座");
    		MULTILINGUAL.put("LJWZP7XVMX", "まいにちイタリア語");
-//      	MULTILINGUAL.put("4411", "まいにちイタリア語_応用編");
  		MULTILINGUAL.put("N8PZRZ9WQY", "まいにちドイツ語");
-// 		MULTILINGUAL.put("4410", "まいにちドイツ語_応用編");
 		MULTILINGUAL.put("XQ487ZM61K", "まいにちフランス語");
-//		MULTILINGUAL.put("4412", "まいにちフランス語_応用編");
 		MULTILINGUAL.put("09NRZWXVGQ19", "まいにちスペイン語");
-//        MULTILINGUAL.put("4413", "まいにちスペイン語_中級編_応用編");
 		MULTILINGUAL.put("YRLK72JZ7Q", "まいにちロシア語");
- //       MULTILINGUAL.put("4414", "まいにちロシア語_応用編");
 		MULTILINGUAL.put("WKMNWGMN6R", "アラビア語講座");
-//		MULTILINGUAL.put("1893", "ポルトガル語講座_入門(前期)");
 		MULTILINGUAL.put("N13V9K157Y", "ポルトガル語");
 		MULTILINGUAL.put("4MY6Q8XP88", "Living_in_Japan");
 		MULTILINGUAL.put("6LPPKP6W8Q", "やさしい日本語");
-		MULTILINGUAL.put("D6RM27PGVM", "Learn_Japanese_from_the_News");
+//		MULTILINGUAL.put("D6RM27PGVM", "Learn_Japanese_from_the_News");
 	}
 
 	protected static ArrayList<Classes> classes;
